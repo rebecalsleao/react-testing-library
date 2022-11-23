bfff2d06bf40ae4c856b3473211e3bcc59d801cd
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-11-23 14:58:14 -0300

    Requsito 4

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react-router-dom';
+// import renderWithRouter from '../renderWithRouter';
 import FavoritePokemon from '../pages/FavoritePokemon';
 
 describe('Teste se o componente Favorite Pokemon renderiza elementos específicos', () => {
@@ -8,4 +9,9 @@ describe('Teste se o componente Favorite Pokemon renderiza elementos específico
     const notFavoritePokemon = screen.getByText(/no favorite pokémon found/i);
     expect(notFavoritePokemon).toBeInTheDocument();
   });
+//   test('Se é exibida na tela a mensagem o card do Pokémon favoritos;', () => {
+//     renderWithRouter(<FavoritePokemon />);
+//     const favoritePokemonCard = screen.getByText(/pikachu/i);
+//     expect(favoritePokemonCard).toBeInTheDocument();
+//   });
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react-router-dom';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testar o componente Not Found', () => {
+  test('Se a página contém um heading h2 com texto específico', () => {
+    renderWithRouter(<NotFound />);
+    const foundTagH2 = screen.getByRole('heading', { level: 2 });
+    const foundTextH2 = screen.getByText('Page requested not found');
+    expect(foundTagH2).toBeInTheDocument();
+    expect(foundTextH2).toBeInTheDocument();
+  });
+  test('Se a página contem o atributo src da imagem', () => {
+    renderWithRouter(<NotFound />);
+    const foundImage = screen.getByRole('img');
+    expect(foundImage.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
