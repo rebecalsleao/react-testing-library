6d0d9013882fd118c457fc84f242f995c0d367ae
Rebeca Souza Leão <rebeca.lsleao@gmail.com>
2022-11-29 14:21:33 -0300

    Requsito 5 - Corrigindo erros de lint

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react-router-dom';
-// import renderWithRouter from '../renderWithRouter';
 import FavoritePokemon from '../pages/FavoritePokemon';
 
 describe('Teste se o componente Favorite Pokemon renderiza elementos específicos', () => {
@@ -9,10 +8,5 @@ describe('Teste se o componente Favorite Pokemon renderiza elementos específico
     const notFavoritePokemon = screen.getByText(/no favorite pokémon found/i);
     expect(notFavoritePokemon).toBeInTheDocument();
   });
-//   test('Se é exibida na tela a mensagem o card do Pokémon favoritos;', () => {
-//     renderWithRouter(<FavoritePokemon />);
-//     const favoritePokemonCard = screen.getByText(/pikachu/i);
-//     expect(favoritePokemonCard).toBeInTheDocument();
-//   });
 });
 
